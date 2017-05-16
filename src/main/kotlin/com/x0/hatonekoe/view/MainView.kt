cd1a15440bd681c9c4b786372bfc885d93118c90
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-05-17 08:54:02 +0900

    Use Japanese font

--- src/main/kotlin/com/x0/hatonekoe/view/MainView.kt
+++ src/main/kotlin/com/x0/hatonekoe/view/MainView.kt
@@ -4,11 +4,16 @@ import com.x0.hatonekoe.controller.MainController
 import com.x0.hatonekoe.service.Constant
 import tornadofx.View
 import tornadofx.borderpane
+import tornadofx.style
 
 class MainView: View(Constant.APP_NAME) {
     private val mainController: MainController by inject()
 
     override val root = borderpane {
+        style {
+            fontFamily = "YuGothic" // Windows は "Yu Gothic"
+        }
+
         top(MenuBarView::class)
         center(FormView::class)
         bottom(FooterView::class)
