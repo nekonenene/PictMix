bc665005ff82e6e5972799b09643c05ab8e14a06
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-05-18 01:14:56 +0900

    Implement embedding the logo of the Idolmaster

---
+++ src/main/kotlin/com/x0/hatonekoe/service/ImageService.kt
@@ -0,0 +1,44 @@
+package com.x0.hatonekoe.service
+
+import org.bytedeco.javacpp.opencv_core.*
+import org.bytedeco.javacpp.opencv_imgcodecs.*
+import org.bytedeco.javacpp.opencv_imgproc.*
+import java.io.File
+
+object ImageService {
+    /**
+     * Open a file
+     */
+    fun addImage(file: File) {
+        val originalImage: Mat = imread(file.absolutePath, IMREAD_UNCHANGED)
+
+        val imasLogoFile = File("imas_logo.png")
+        val imasLogoImage: Mat = imread(imasLogoFile.absolutePath, IMREAD_UNCHANGED)
+
+        val originalImageWidth: Double = originalImage.size().width().toDouble()
+        val logoImageWidth: Double = imasLogoImage.size().width().toDouble()
+        val imasLogoResizeRatio: Double = originalImageWidth / logoImageWidth / 2.0
+
+        val resizedLogoImage: Mat = Mat()
+        resize(imasLogoImage, resizedLogoImage, Size(), imasLogoResizeRatio, imasLogoResizeRatio, INTER_LINEAR)
+
+        val ctr = Point2f(originalImage.cols().toFloat(), originalImage.rows().toFloat())
+        val mat: Mat = getRotationMatrix2D(ctr, 0.0, 1.0)
+
+        warpAffine(resizedLogoImage, originalImage, mat, originalImage.size(), CV_INTER_LINEAR, BORDER_TRANSPARENT, null)
+
+        imwrite(file.parent + "/" + removeExtension(file) + "_imas.png", originalImage)
+    }
+
+    fun removeExtension(file: File): String {
+        val fileName = file.name
+        val index = fileName.lastIndexOf('.')
+
+        if (index != -1) {
+            return fileName.substring(0, index)
+        } else {
+            // return original file name if doesn't have an extension
+            return fileName
+        }
+    }
+}
--- src/main/kotlin/com/x0/hatonekoe/view/FormView.kt
+++ src/main/kotlin/com/x0/hatonekoe/view/FormView.kt
@@ -1,22 +1,28 @@
 package com.x0.hatonekoe.view
 
+import com.x0.hatonekoe.service.ImageService
 import javafx.geometry.Pos
 import tornadofx.*
+import java.io.File
 
 class FormView : View() {
     override val root = form {
+        val input1 = FilePathInputView("Picture A")
+
         alignment = Pos.CENTER
 
         fieldset {
             alignment = Pos.TOP_LEFT
 
-            this += FilePathInputView("Picture A")
+
+            this += input1
             this += FilePathInputView("Picture B")
         }
 
         button("Create!") {
             action {
-                println("creating now...")
+                val file = File(input1.filePathField.text)
+                ImageService.addImage(file)
             }
         }
     }
